9e94c9fc820c57bca8f3d66da805a03b0fbd2fce
maordan <maor.dn@gmail.com>
2019-04-23 00:31:16 +0300

    adapted report table to be whole year/half/quart oriented

--- expense_site/expense/views.py
+++ expense_site/expense/views.py
@@ -32,9 +32,9 @@ TWO_YEARS_AGO = CURRENT_YEAR - 2
 
 LAST_MONTH = ((THIS_MONTH - 1 - 1) % 12) + 1
 
-LAST_QUART = ((LAST_MONTH - 1)//3) + 1
+LAST_QUART = ((LAST_MONTH - 3)//3) % 4 + 1
 
-LAST_HALF = ((LAST_MONTH - 1)//6) + 1
+LAST_HALF = ((LAST_MONTH - 6)//6) % 2 + 1
 
 two_years_ago_start = datetime(TWO_YEARS_AGO, 1, 1)
 two_years_ago_end = datetime(TWO_YEARS_AGO+1, 1, 1)
@@ -52,21 +52,27 @@ last_month_start = datetime(LAST_YEAR if LAST_MONTH == 12 else CURRENT_YEAR, LAS
 last_month_end = datetime(CURRENT_YEAR, (LAST_MONTH % 12) + 1, 1)
 
 def details(request):
-    #TODO: fix buttons to new date format
     print (request.GET)
+    start_date = {1 : last_month_start,
+                      3 : last_quart_start,
+                      6 : last_half_start,
+                      12: last_year_start}[int(request.GET['monthsback'])]
+    end_date =   {1 : last_month_end,
+                      3 : last_quart_end,
+                      6 : last_half_end,
+                      12: last_year_end}[int(request.GET['monthsback'])]
     if int(request.GET['cat']) == -1:
         return render(request, 'details/details.html',
                   {'details': Expense.objects.raw(r"SELECT * from expense  LEFT JOIN names on expense.name = names.name LEFT JOIN files on expense.file_id = files.id LEFT JOIN Category on names.cat = Category.id"
                                                   r" where (names.name is null or Category.id = 0) "
-                                                  r" and expense.date > date('%s','start of month','-%s month') "
+                                                  r" and expense.date > '%s' "
                                                   r" and expense.date < '%s'"
-                                                  r" ORDER BY date"%(datetime.now().strftime("%Y-%m-%d"),request.GET['monthsback'],datetime.now().strftime("%Y-%m-%d")))
+                                                  r" ORDER BY date"%(start_date, end_date))
                    })
 
 
     return render(request, 'details/details.html',
-                  {'details': Expense.objects.filter(date__lt=datetime.now(),date__gte= datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                                                relativedelta(months=int(request.GET['monthsback'])),
+                  {'details': Expense.objects.filter(date__lt=end_date, date__gte= start_date,
                                                      name__cat__id=int(request.GET['cat'])).order_by('date')
                    })
 
@@ -109,75 +115,63 @@ def missing(request):
 #& Q(name__isnull=False)
 def report(request):
     sums= Category.objects.filter(Q(id__gt=0,names__expense__date__lt=datetime.now())).annotate(
-        lastMonth= Sum(Case(When(names__expense__date__gte=
-                                 datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                 relativedelta(months=1),
+        lastMonth= Sum(Case(When(names__expense__date__gte=last_month_start,
+                                 names__expense__date__lt=last_month_end,
                                  names__expense__charge_number=1,
                                  then=F('names__expense__charge')*F('names__expense__total_charges')))),
-        lastQuarter= Sum(Case(When(names__expense__date__gte=
-                                   datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                   relativedelta(months=3),
+        lastQuarter= Sum(Case(When(names__expense__date__gte=last_quart_start,
+                                   names__expense__date__lt=last_quart_end,
                                    names__expense__charge_number=1,
                                    then=F('names__expense__charge')*F('names__expense__total_charges')))),
-        lastHalf= Sum(Case(When(names__expense__date__gte=
-                                datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                relativedelta(months=6),
+        lastHalf= Sum(Case(When(names__expense__date__gte=last_half_start,
+                                names__expense__date__lt=last_half_end,
                                 names__expense__charge_number=1,
                                 then=F('names__expense__charge')*F('names__expense__total_charges')))),
-        lastYear= Sum(Case(When(names__expense__date__gte=
-                                datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                relativedelta(months=12),
+        lastYear= Sum(Case(When(names__expense__date__gte=last_year_start,
+                                names__expense__date__lt=last_year_end,
                                 names__expense__charge_number=1,
                                 then=F('names__expense__charge')*F('names__expense__total_charges')))))
 
 
     totals = Expense.objects.filter(~Q(id=-1) and Q(date__lt=datetime.now())).aggregate(
-                       lastMonth=Sum(Case(When(date__gte=
-                                               datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                               relativedelta(months=1),
+                       lastMonth=Sum(Case(When(date__gte=last_month_start,
+                                               date__lt=last_month_end,
                                                charge_number=1,
                                                then=F('charge')*F('total_charges')))),
-                       lastQuarter= Sum(Case(When(date__gte=
-                                                  datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                                  relativedelta(months=3),
+                       lastQuarter= Sum(Case(When(date__gte=last_quart_start,
+                                                  date__lt=last_quart_end,
                                                   charge_number=1,
                                                   then=F('charge')*F('total_charges')))),
-                       lastHalf= Sum(Case(When(date__gte=
-                                               datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                               relativedelta(months=6),
+                       lastHalf= Sum(Case(When(date__gte=last_half_start,
+                                               date__lt=last_half_end,
                                                charge_number=1,
                                                then=F('charge')*F('total_charges')))),
-                       lastYear= Sum(Case(When(date__gte=
-                                               datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                               relativedelta(months=12),
+                       lastYear= Sum(Case(When(date__gte=last_year_start,
+                                               date__lt=last_year_end,
                                                charge_number=1,
                                                then=F('charge')*F('total_charges')))))
 
     not_categorized = Expense.objects.filter(~Q(id=-1)and Q(date__lt=datetime.now())).aggregate(
-        lastMonth= totals['lastMonth'] - Sum(Case(When(date__gte=
-                                 datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                 relativedelta(months=1),
-                                 charge_number=1,
-                                 name__cat__isnull=False,
-                                 then=F('charge')*F('total_charges')))),
-        lastQuarter= totals['lastQuarter'] - Sum(Case(When(date__gte=
-                                 datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                 relativedelta(months=3),
-                                 charge_number=1,
-                                 name__cat__isnull=False,
-                                 then=F('charge')*F('total_charges')))),
-        lastHalf= totals['lastHalf'] - Sum(Case(When(date__gte=
-                                 datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                 relativedelta(months=6),
-                                 charge_number=1,
-                                 name__cat__isnull=False,
-                                 then=F('charge')*F('total_charges')))),
-        lastYear= totals['lastYear'] - Sum(Case(When(date__gte=
-                                 datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                                 relativedelta(months=12),
-                                 charge_number=1,
-                                 name__cat__isnull=False,
-                                 then=F('charge')*F('total_charges')))),
+        lastMonth= totals['lastMonth'] - Sum(Case(When(date__gte=last_month_start,
+                                                       date__lt=last_month_end,
+                                                       charge_number=1,
+                                                       name__cat__isnull=False,
+                                                       then=F('charge')*F('total_charges')))),
+        lastQuarter= totals['lastQuarter'] - Sum(Case(When(date__gte=last_quart_start,
+                                                           date__lt=last_quart_end,
+                                                           charge_number=1,
+                                                           name__cat__isnull=False,
+                                                           then=F('charge')*F('total_charges')))),
+        lastHalf= totals['lastHalf'] - Sum(Case(When(date__gte=last_half_start,
+                                                     date__lt=last_half_end,
+                                                     charge_number=1,
+                                                     name__cat__isnull=False,
+                                                     then=F('charge')*F('total_charges')))),
+        lastYear= totals['lastYear'] - Sum(Case(When(date__gte=last_year_start,
+                                                     date__lt=last_year_end,
+                                                     charge_number=1,
+                                                     name__cat__isnull=False,
+                                                     then=F('charge')*F('total_charges')))),
 
     )
 
